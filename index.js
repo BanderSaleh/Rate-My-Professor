4709cfac73d60a6ef35dee8825b5f58cd121f5ce
PeterH <pete08@gmail.com>
2020-12-28 20:55:27 -0600

    Added functionality to Create Professor, Update Professor, Delete Professor, Create Review, Update Review, Delete Review

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const db = require('./queries')
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const db = require('./queries');
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(cors());
+
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+});
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
