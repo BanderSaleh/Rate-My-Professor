2d8c78553a8490c1202d20d5a25f73defb2cb77f
Mendel Cohen <mmhakohen@gmail.com>
2021-02-28 14:33:01 -0500

    updates port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const db = require('./queries');
-const port = 3000;
+const port = process.env.PORT || 3000;
 var jwt = require('jsonwebtoken');
 
 app.use(cors());
